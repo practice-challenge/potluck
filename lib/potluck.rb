97106e9e84c5cf4f46a8121d77fef983709120f1
Ryan Camp <campryan@comcast.net>
2020-09-18 17:57:19 -0600

    Write and pass test to check menu display for Potluck class

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -15,4 +15,18 @@ class Potluck
       dish.category == category
     end
   end
+
+  def menu
+    categorized_menu = @dishes.reduce({}) do |menu, dish|
+      menu[dish.category] = [] if menu[dish.category].nil?
+      menu[dish.category] << dish.name
+      menu
+    end
+
+    pluralize_categories = categorized_menu.transform_keys do |key|
+      (key.to_s << 's').to_sym
+    end
+
+    pluralize_categories.transform_values(&:sort)
+  end
 end
--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -44,4 +44,25 @@ RSpec.describe Potluck do
     expect(@potluck.get_all_from_category(:appetizer).first).to be(couscous_salad)
     expect(@potluck.get_all_from_category(:appetizer).first.name).to eql("Couscous Salad")
   end
+
+  it "can return a hash by category as a menu" do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    bean_dip = Dish.new("Bean Dip", :appetizer)
+    @potluck.add_dish(couscous_salad)
+    @potluck.add_dish(summer_pizza)
+    @potluck.add_dish(roast_pork)
+    @potluck.add_dish(cocktail_meatballs)
+    @potluck.add_dish(candy_salad)
+    @potluck.add_dish(bean_dip)
+
+    expect(@potluck.menu).to eql({
+      :appetizers=>["Bean Dip", "Couscous Salad", "Summer Pizza"],
+      :entres=>["Cocktail Meatballs", "Roast Pork"],
+      :desserts=>["Candy Salad"]
+      })
+  end
 end
