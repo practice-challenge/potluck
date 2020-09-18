71c1aa43ff191ef7b2e3f96447d41647bd6e796e
Ryan Camp <campryan@comcast.net>
2020-09-18 17:03:22 -0600

    Write and pass test to add dish to collection in Potluck class

--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe Potluck do
     expect(@potluck.date).to eql("7-13-18")
     expect(@potluck.dishes).to eql([])
   end
+
+  it "can add dish to dishes collection" do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+
+    expect(@potluck.dishes).to eql([])
+
+    @potluck.add_dish(couscous_salad)
+
+    expect(@potluck.dishes).to eql([couscous_salad])
+
+    @potluck.add_dish(cocktail_meatballs)
+
+    expect(@potluck.dishes).to eql([couscous_salad, cocktail_meatballs])
+    expect(@potluck.dishes.length).to eql(2)
+
+  end
 end
