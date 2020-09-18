d6d6592f6cb8d642d9cd80a0abd881ce6aa1034b
Ryan Camp <campryan@comcast.net>
2020-09-18 17:15:21 -0600

    Write and pass test to get all dishes from category

--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -26,6 +26,22 @@ RSpec.describe Potluck do
 
     expect(@potluck.dishes).to eql([couscous_salad, cocktail_meatballs])
     expect(@potluck.dishes.length).to eql(2)
+  end
 
+  it "can get all dishes from category" do
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+    @potluck.add_dish(couscous_salad)
+    @potluck.add_dish(summer_pizza)
+    @potluck.add_dish(roast_pork)
+    @potluck.add_dish(cocktail_meatballs)
+    @potluck.add_dish(candy_salad)
+
+    expect(@potluck.get_all_from_category(:appetizer)).to contain_exactly(couscous_salad, summer_pizza)
+    expect(@potluck.get_all_from_category(:appetizer).first).to be(couscous_salad)
+    expect(@potluck.get_all_from_category(:appetizer).first.name).to eql("Couscous Salad")
   end
 end
