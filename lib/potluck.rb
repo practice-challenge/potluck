d34301c9cf085d8d084ce6f354d06cfbd5416765
Ryan Camp <campryan@comcast.net>
2020-09-18 16:57:44 -0600

    Write and pass attributes test for Potluck class

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,5 +1,9 @@
 class Potluck
+  attr_reader :date, :dishes
+  
   def initialize(date)
-
+    @date = date
+    @dishes = []
   end
+
 end
--- spec/potluck_spec.rb
+++ spec/potluck_spec.rb
@@ -2,8 +2,13 @@ RSpec.describe Potluck do
   before do
     @potluck = Potluck.new("7-13-18")
   end
-  
+
   it "exists" do
     expect(@potluck).to be_an_instance_of(Potluck)
+  end
+
+  it "has readable attributes" do
+    expect(@potluck.date).to eql("7-13-18")
+    expect(@potluck.dishes).to eql([])
   end
 end
